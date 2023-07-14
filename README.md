3425cc1c36fb80de87b334c068aec81e92fe8620
Andis Redmans <andis.redmans@sap.com>
2023-07-14 17:06:42 +0300

    feat: new scenario
    new scenario

--- README.md
+++ README.md
@@ -1,20 +1,11 @@
 # MemFS
 
-Original example from Microsoft - 
-https://github.com/microsoft/vscode-extension-samples/tree/main/fsprovider-sample
-It was modified to meet condition for issue reproduction
-
-Step to reproduce issue(MS Windows was used):
-
 - install this extension
 - Run extension
-- Open any folder on your PC.
-- Execute command `Create Virtual File and Open`("memfs.init"). Command does following:
-    - Command creates virtual file based on opened folder path by appending `dummy/subfolder/test.txt'`.
-    - Command opens TextEditor for created virtual file .
-- Change content of file and save
+- Open any folder in opened extension window
+- add zip file from `temp/testProject.zip`.
+- run command `watcherTest: Register global watcher` to register global watchers to monitor `**/*.js` file changes.
+- Extract `temp/testProject.zip` in opened extension window by running command `unzip estProject.zip`.
+- Change js file from extracted sources -> `testProject\ext\test.js`
 
-Problem, watcher triggers are not executed. Info message should appear if watcher triggers(`showInformationMessage` is used in watcher).
-Following watchers were used - https://github.com/815are/virtualFileWatchIssue/blob/main/src/extension.ts#L70-L77
-
-Scenario work fine in VSCode 1.63.2 and below. Issue happens only in VSCode Insiders 1.64.0
+Problem, watcher is not triggered in BAS.
--- src/extension.ts
+++ src/extension.ts
@@ -1,136 +1,66 @@
 "use strict";
 
 import * as vscode from "vscode";
-import { posix } from "path";
+import { join } from "path";
 import { TextDecoder } from "util";
-import { MemFS } from "./fileSystemProvider";
 import { startTime, stopTime } from "./time";
 
-enum SchemeName {
-  Memfs = "memfs",
-}
-
-function fsPathToFtfsUri(path: string): vscode.Uri {
-  let posixPath = path.replace(/\\/g, "/");
-  if (!posix.isAbsolute(posixPath)) {
-    posixPath = `/${posixPath}`;
-  }
-  return vscode.Uri.parse(`${SchemeName.Memfs}:${posixPath}`);
-}
-
-async function openTextDocument(uri: vscode.Uri): Promise<void> {
-  const openUri = fsPathToFtfsUri(uri.fsPath);
-  const document: vscode.TextDocument = await vscode.workspace.openTextDocument(
-    openUri
-  );
-  await vscode.window.showTextDocument(document, vscode.ViewColumn.One);
-}
-
 export function activate(context: vscode.ExtensionContext) {
-  console.log('MemFS says "Hello"');
+  console.log('Test says "Hello"');
 
-  const memFs = new MemFS();
-  context.subscriptions.push(
-    vscode.workspace.registerFileSystemProvider(SchemeName.Memfs, memFs, {
-      isCaseSensitive: true,
-    })
-  );
-  let initialized = false;
-
-  context.subscriptions.push(
-    vscode.commands.registerCommand("memfs.reset", (_) => {
-      for (const [name] of memFs.readDirectory(
-        vscode.Uri.parse(`${SchemeName.Memfs}:/`)
-      )) {
-        memFs.delete(vscode.Uri.parse(`${SchemeName.Memfs}:/${name}`));
-      }
-      initialized = false;
-    })
-  );
-
-  context.subscriptions.push(
-    vscode.commands.registerCommand("memfs.init", async (_) => {
-      // Get fist workspace folder
-      const workspaceUri = vscode.workspace.workspaceFolders?.[0]
-        ?.uri as vscode.Uri;
-      if (!workspaceUri) {
-        vscode.window.showErrorMessage(
-          "Please open any folder for issue reproduction"
-        );
-      }
-
-      // Create virtual file
-      const fileUri = workspaceUri.with({
-        path: posix.join("dummy", "subfolder", "test.txt"),
-      });
-
-      if (initialized) {
-        await openTextDocument(fileUri);
-        return;
-      }
-      initialized = true;
-
-      memFs.writeFile(fileUri, Buffer.from("Test data"), {
-        create: true,
-        overwrite: true,
-      });
-
-      // Try to add watchers
-      const sourceFileWatch = vscode.workspace.createFileSystemWatcher(
-        "**" + fileUri.path
-      );
-      sourceFileWatch.onDidChange(() => {
-        vscode.window.showInformationMessage("File changed");
-      });
-      const sourceFileWatch2 = vscode.workspace.createFileSystemWatcher(
-        fileUri.path
-      );
-      sourceFileWatch2.onDidChange(() => {
-        vscode.window.showInformationMessage("File changed 2");
-      });
-
-      // Open newly created virtual
-      await openTextDocument(fileUri);
-    })
-  );
-
-  const readFile = posix.join("dummy", "subfolder", "test.txt");
-  const parseFile = (content: Uint8Array): string => {
-    return new TextDecoder("utf-8").decode(content);
+  const changeCB = (path: string, prefix: string) => {
+    vscode.window.showInformationMessage(
+      `${prefix} Change triggered for file ${path}`
+    );
   };
+
+  const createCB = (path: string, prefix: string) => {
+    vscode.window.showInformationMessage(
+      `${prefix} Create triggered for file ${path}`
+    );
+  };
+
+  // Command to register global handler to listen all xml files
   context.subscriptions.push(
-    vscode.commands.registerCommand("memfs.readDirect", async (_) => {
-      const readFilenUri = fsPathToFtfsUri(readFile);
-      startTime("readDirect");
-      const file = await memFs.readFile(readFilenUri);
-      const timeEnd = stopTime("readDirect");
-      await vscode.window.showInformationMessage(
-        `Direct read from provider. Time - ${timeEnd}ms. File content: \n "${parseFile(
-          file
-        )}"`
-      );
+    vscode.commands.registerCommand("watcherTest.global", (_) => {
+      const patternPath = "**/*.js";
+      const watch: vscode.FileSystemWatcher =
+        vscode.workspace.createFileSystemWatcher(patternPath);
+      watch.onDidChange((uri: vscode.Uri) => {
+        changeCB(uri.fsPath, "GLOBAL1");
+      });
+      watch.onDidCreate((uri: vscode.Uri) => {
+        createCB(uri.fsPath, "GLOBAL1");
+      });
+      if (vscode.workspace.workspaceFolders) {
+        const relativePattern = new vscode.RelativePattern(
+          vscode.workspace.workspaceFolders[0],
+          "**/*.js"
+        );
+        const watch2: vscode.FileSystemWatcher =
+          vscode.workspace.createFileSystemWatcher(relativePattern);
+        watch2.onDidChange((uri: vscode.Uri) => {
+          changeCB(uri.fsPath, "GLOBAL2");
+        });
+        watch2.onDidCreate((uri: vscode.Uri) => {
+          createCB(uri.fsPath, "GLOBAL2");
+        });
+        vscode.window.showInformationMessage("Watcher registered");
+      }
     })
   );
 
+  // Command to register global handler to listen all xml files
   context.subscriptions.push(
-    vscode.commands.registerCommand("memfs.readWorkspace", async (_) => {
-      const readFilenUri = fsPathToFtfsUri(readFile);
-      startTime("readWorkspace");
-      const file = await vscode.workspace["fs"].readFile(readFilenUri);
-      const timeEnd = stopTime("readWorkspace");
-      await vscode.window.showInformationMessage(
-        `Read using workspace API. Time - ${timeEnd}ms. File content: \n "${parseFile(
-          file
-        )}"`
+    vscode.commands.registerCommand("watcherTest.local", (context) => {
+      const watch = vscode.workspace.createFileSystemWatcher(
+        new vscode.RelativePattern(context.fsPath, "*")
       );
-    })
-  );
-
-  context.subscriptions.push(
-    vscode.commands.registerCommand("memfs.workspaceInit", (_) => {
-      vscode.workspace.updateWorkspaceFolders(0, 0, {
-        uri: vscode.Uri.parse(`${SchemeName.Memfs}:/`),
-        name: "MemFS - Sample",
+      watch.onDidChange((uri: vscode.Uri) => {
+        changeCB(uri.fsPath, "LOCAL");
+      });
+      watch.onDidCreate((uri: vscode.Uri) => {
+        createCB(uri.fsPath, "LOCAL");
       });
     })
   );
